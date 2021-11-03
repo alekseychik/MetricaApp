29864bb97eb68739f08b890800e8d958c8332e01
alekseychik <oleksii.belenchuk@gmail.com>
2021-11-03 22:19:14 +0200

    initial-upload

--- README.md
+++ README.md
@@ -1,102 +1,27 @@
-# [Admin One &mdash; Free Tailwind Admin Dashboard](https://justboil.me/tailwind-admin-templates)
+# SpicaAdmin-Free-Bootstrap-Admin-Template
 
-[![version](https://img.shields.io/github/v/release/justboil/admin-one-tailwind)](https://justboil.me/tailwind-admin-templates)  [![license](https://img.shields.io/badge/license-MIT-blue.svg)](https://justboil.me/tailwind-admin-templates)
+Spica Admin is a simple yet powerful free Bootstrap admin template that is built with the Bootstrap framework, SASS, jQuery, HTML5, and CSS. The spectacular design, user friendliness, and easy to customizability of Spica Admin makes it suitable for building different types of admin websites. This well documented template has high responsiveness to make websites look great on both desktop and mobile devices.
 
-[![Free Tailwind CSS admin dashboard](https://justboil.me/images/one-tailwind/repository-preview-hi-res.png)](https://justboil.github.io/admin-one-tailwind/)
+The wide array of useful Dashboard elements and other essential features available with this template will make the process of website creation and customization a lot simpler. The clean and well-written code of Spica Admin Template is easy to comprehend even for beginners.
 
-**Admin One** is simple, beautiful and free Tailwind CSS admin dashboard
+Take a quick glance at the preview of Spica Admin template [here](http://www.bootstrapdash.com/demo/spica-free/template/index.html)
 
-* Free under MIT License
-* Built with Tailwind CSS Framework
-* Pure HTML & CSS
-* No js framework dependencies
-* Ready-to-use CSS files
-* [Premium Vue.js 3 version](https://justboil.me/tailwind-admin-templates/vue-dashboard) available
+[![N|Solid](preview.jpg)](http://www.bootstrapdash.com/demo/spica-free/template/index.html)
 
-## Table of Contents
 
-* [Other versions](#other-versions)
-* [Demo](#demo)
-* [Quick Start](#quick-start)
-* [Browser Support](#browser-support)
-* [Reporting Issues](#reporting-issues)
-* [Licensing](#licensing)
-* [Useful Links](#useful-links)
+<h2>How to use </h2>
 
-## Other versions
-
-This is Pure HTML/CSS Tailwind CSS admin dashboard version
-
-<table>
-    <tr>
-        <td align="center" colspan="2"><a href="https://justboil.me/tailwind-admin-templates"><img src="https://justboil.me/images/tailwind-gh-logo.png?v=2" width="219" height="40" alt="Tailwind CSS admin dashboard templates"></a></td>
-    </tr>
-    <tr>
-        <td align="center"><a href="https://github.com/justboil/admin-one-tailwind" title="Free Tailwind CSS admin dashboard HTML"><img src="https://justboil.me/svg/language-html5.svg" width="64" height="64"></a></td>
-        <td align="center"><a href="https://github.com/justboil/admin-one-vue-tailwind" title="Free Vue.js 3 Tailwind CSS admin dashboard"><img src="https://justboil.me/svg/vuejs.svg" width="64" height="64"></a></td>
-    </tr>
-    <tr>
-        <td align="center">Tailwind admin dashboard<br/>Pure HTML/CSS<br/><br/><a href="https://github.com/justboil/admin-one-tailwind" title="Free Tailwind admin dashboard HTML CSS">Free</a></td>
-        <td align="center">Tailwind admin dashboard<br/>Vue.js 3<br/><br/><a href="https://github.com/justboil/admin-one-vue-tailwind" title="Free Vue.js 3 Tailwind CSS admin dashboard">Free</a> | <a href="https://justboil.me/tailwind-admin-templates/vue-dashboard" title="Vue.js 3 Tailwind CSS admin dashboard">Premium</a></td>
-    </tr>
-</table>
-
-## Demo
-
-#### Free dashboard demo
-
-https://justboil.github.io/admin-one-tailwind/
-
-#### Premium Vue.js 3 dashboard demo
-
-https://tailwind-vue.justboil.me/
-
-## Quick Start 
-
-#### Get the repo
-
-* [Create new repo](https://github.com/justboil/admin-one-tailwind/generate) from this template
-* &hellip; or clone the repo on GitHub
-* &hellip; or [download .zip](https://github.com/justboil/admin-one-tailwind/archive/master.zip) from GitHub
-
-#### HTML & CSS
-
-Check `dist` directory.
-
-#### npm tools
-
-##### Install
-
-`cd` to project's dir and run `npm install` 
-
-##### Build
-
-`npm run build` to rebuild `dist` from sources in `src` directory
-
-## Browser Support
-
-We try to make sure Dashboard works well in the latest versions of all major browsers
-
-<img src="https://justboil.me/images/browsers-svg/chrome.svg" width="64" height="64" alt="Chrome"> <img src="https://justboil.me/images/browsers-svg/firefox.svg" width="64" height="64" alt="Firefox"> <img src="https://justboil.me/images/browsers-svg/edge.svg" width="64" height="64" alt="Edge"> <img src="https://justboil.me/images/browsers-svg/safari.svg" width="64" height="64" alt="Safari"> <img src="https://justboil.me/images/browsers-svg/opera.svg" width="64" height="64" alt="Opera">
-
-## Reporting Issues
-
-JustBoil's free items are limited to community support on GitHub.
-
-The issue list is reserved exclusively for bug reports and feature requests. That means we do not accept usage questions. If you open an issue that does not conform to the requirements, it will be closed.
-
-1. Make sure that you are using the latest version of the Dashboard. Issues for outdated versions are irrelevant
-2. Provide steps to reproduce
-3. Provide an expected behavior
-4. Describe what is actually happening 
-5. Platform, Browser & version as some issues may be browser specific
-
-## Licensing
-
-- Copyright &copy; 2019-2021 JustBoil.me (https://justboil.me)
-- Licensed under MIT
-
-## Useful Links
-
-- [JustBoil.me](https://justboil.me/)
-- [TailwindCSS](https://tailwindcss.com/)
+<ul>
+  <li>
+    Download or Clone the repositary
+  </li>
+  <li>
+    Open the file template/index.html in a browser to view the demo
+  </li>
+  <li>
+    Start editing the code as per your requirement
+  </li>
+  <li>
+    If you wish to customize the template, use the command 'npm install' to install the necessary dependencies. Refer <a href="http://www.bootstrapdash.com/demo/spica-free/template/docs/documentation.html">documentaion</a> for more details on customization.
+  </li>
+</ul>
--- gulpfile.js
+++ gulpfile.js
@@ -1,153 +1,161 @@
-const {series, parallel, src, dest} = require('gulp')
-const rename = require('gulp-rename')
-const babel = require('gulp-babel')
-const concat = require('gulp-concat')
-const replace = require('gulp-replace')
-const uglify = require('gulp-uglify')
-const postcss = require('gulp-postcss')
-const cleancss = require('gulp-clean-css')
+'use strict'
 
-/* Destination dir */
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var del = require('del');
+var replace = require('gulp-replace');
+var injectPartials = require('gulp-inject-partials');
+var inject = require('gulp-inject');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var merge = require('merge-stream');
 
-const destDir = './dist'
 
-/* JS. Transpile with babel & minify */
 
-const processJs = (baseName, isMin) => {
-  let r = src('src/js/' + baseName + '.js')
-    .pipe(babel({
-      presets: ['@babel/env']
-    }))
+gulp.task('sass', function () {
+    return gulp.src('./scss/style.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
+});
 
-  if (isMin) {
-    r = r.pipe(uglify()).pipe(rename(baseName + '.min.js'))
-  }
+// Static Server + watching scss/html files
+gulp.task('serve', gulp.series('sass', function() {
 
-  return r.pipe(dest(destDir + '/js'))
-}
+    browserSync.init({
+        port: 3000,
+        server: "./",
+        ghostMode: false,
+        notify: false
+    });
 
-const processJsMain = () => {
-  return processJs('main')
-}
+    gulp.watch('scss/**/*.scss', gulp.series('sass'));
+    gulp.watch('**/*.html').on('change', browserSync.reload);
+    gulp.watch('js/**/*.js').on('change', browserSync.reload);
 
-const processJsMainMin = () => {
-  return processJs('main', true)
-}
+}));
 
-const processJsChartSample = () => {
-  return processJs('chart.sample')
-}
 
-const processJsChartSampleMin = () => {
-  return processJs('chart.sample', true)
-}
 
-/* CSS */
+// Static Server without watching scss files
+gulp.task('serve:lite', function() {
 
-const processCss = () => {
-  return src('src/css/main.css')
-    .pipe(postcss())
-    .pipe(cleancss())
-    .pipe(dest(destDir + '/css'))
-}
+    browserSync.init({
+        server: "./",
+        ghostMode: false,
+        notify: false
+    });
 
-/* HTML */
-const dateNowMillis = Date.now()
+    gulp.watch('**/*.css').on('change', browserSync.reload);
+    gulp.watch('**/*.html').on('change', browserSync.reload);
+    gulp.watch('js/**/*.js').on('change', browserSync.reload);
 
-const concatHtml = file => {
-  let replaceHtmlClassWith = ''
-  const formScreenFiles = ['login']
+});
 
-  if (formScreenFiles.indexOf(file) > -1) {
-    replaceHtmlClassWith = 'form-screen'
-  }
 
-  const sources = [
-    'src/html/parts/head.html',
-    'src/html/parts/app-open.html'
-  ]
+/* inject partials like sidebar and navbar */
+gulp.task('injectPartial', function () {
+  return gulp.src("./**/*.html", { base: "./" })
+    .pipe(injectPartials())
+    .pipe(gulp.dest("."));
+});
 
-  if (formScreenFiles.indexOf(file) < 0) {
-    sources.push(
-      'src/html/parts/navbar.html',
-      'src/html/parts/aside.html',
-      'src/html/parts/title-bar.html',
-      'src/html/parts/hero-bar.html'
-    )
-  }
 
-  sources.push(`src/html/${file}.html`)
 
-  if (formScreenFiles.indexOf(file) < 0) {
-    sources.push(
-      'src/html/parts/footer.html',
-      'src/html/parts/sample-modal.html'
-    )
-  }
+/* inject Js and CCS assets into HTML */
+gulp.task('injectCommonAssets', function () {
+  return gulp.src('./**/*.html')
+    .pipe(inject(gulp.src([ 
+        './vendors/mdi/css/materialdesignicons.min.css',
+        './vendors/css/vendor.bundle.base.css', 
+        './vendors/js/vendor.bundle.base.js',
+    ], {read: false}), {name: 'base', relative: true}))
+    .pipe(inject(gulp.src([
+        './css/*.css', 
+        './js/off-canvas.js', 
+        './js/hoverable-collapse.js',
+        './js/template.js',
+    ], {read: false}), {relative: true}))
+    .pipe(gulp.dest('.'));
+});
 
-  sources.push(
-    'src/html/parts/app-close.html',
-    'src/html/parts/bottom-scripts.html'
-  )
+/* inject Js and CCS assets into HTML */
+gulp.task('injectLayoutStyles', function () {
+    var verticalLightStream = gulp.src(['./**/**/*.html',
+            './index.html'])
+        .pipe(inject(gulp.src([
+        './css/style.css', 
+        ], {read: false}), {relative: true}))
+        .pipe(gulp.dest('.'));
+    return merge(verticalLightStream);
+});
 
-  if (file === 'index') {
-    sources.push('src/html/parts/bottom-scripts-home.html')
-  }
+/*replace image path and linking after injection*/
+gulp.task('replacePath', function(){
+    var replacePath1 = gulp.src(['./pages/*/*.html'], { base: "./" })
+        .pipe(replace('="images/', '="../../images/'))
+        .pipe(replace('href="pages/', 'href="../../pages/'))
+        .pipe(replace('href="index.html"', 'href="../../index.html"'))
+        .pipe(replace('href="docs/', 'href="../../docs/'))
+        .pipe(gulp.dest('.'));
+    var replacePath2 = gulp.src(['./*.html'], { base: "./" })
+        .pipe(replace('="images/', '="images/'))
+        .pipe(replace('"pages/', '"pages/'))
+        .pipe(replace('href="index.html"', 'href="index.html"'))
+        .pipe(replace('"docs/', '"docs/'))
+        .pipe(gulp.dest('.'));
+    return merge(replacePath1, replacePath2);
+});
 
-  sources.push('src/html/parts/bottom.html')
+/*sequence for injecting partials and replacing paths*/
+gulp.task('inject', gulp.series('injectPartial' , 'injectCommonAssets' , 'injectLayoutStyles', 'replacePath'));
 
-  const titleStrings = {
-    tables: 'Tables',
-    index: 'Dashboard',
-    forms: 'Forms',
-    profile: 'Profile',
-    login: 'Login'
-  }
+gulp.task('clean:vendors', function () {
+    return del([
+      'vendors/**/*'
+    ]);
+});
 
-  const titleStringsLong = {
-    tables: 'Responsive Tables'
-  }
+/*Building vendor scripts needed for basic template rendering*/
+gulp.task('buildBaseVendorScripts', function() {
+    return gulp.src([
+        './node_modules/jquery/dist/jquery.min.js', 
+        './node_modules/popper.js/dist/umd/popper.min.js', 
+        './node_modules/bootstrap/dist/js/bootstrap.min.js', 
+        './node_modules/perfect-scrollbar/dist/perfect-scrollbar.min.js'
+    ])
+      .pipe(concat('vendor.bundle.base.js'))
+      .pipe(gulp.dest('./vendors/js'));
+});
 
-  const titleStringReplacement = titleStrings[file] ? titleStrings[file] : ''
-  const titleStringLongReplacement = titleStringsLong[file] ? titleStringsLong[file] : titleStringReplacement
+/*Building vendor styles needed for basic template rendering*/
+gulp.task('buildBaseVendorStyles', function() {
+    return gulp.src(['./node_modules/perfect-scrollbar/css/perfect-scrollbar.css'])
+      .pipe(concat('vendor.bundle.base.css'))
+      .pipe(gulp.dest('./vendors/css'));
+});
 
-  return src(sources)
-    .pipe(concat(`${file}.html`))
-    .pipe(replace('--date-now-millis', dateNowMillis.toString()))
-    .pipe(replace('--html-class', replaceHtmlClassWith))
-    .pipe(replace('--stylesheet-min-path', `css/main.css?v=${dateNowMillis.toString()}`))
-    .pipe(replace(`--set-active-${file}-html`, 'active'))
-    .pipe(replace(/ --set-active-[a-z-]+/gi, ''))
-    .pipe(replace('{{ titleString }}', titleStringReplacement))
-    .pipe(replace('{{ titleStringLong }}', titleStringLongReplacement))
-    .pipe(dest(destDir))
-}
+/*Scripts for addons*/
+gulp.task('copyAddonsScripts', function() {
+    var aScript1 = gulp.src(['node_modules/chart.js/dist/Chart.min.js'])
+        .pipe(gulp.dest('./vendors/chart.js'));
+    return merge(aScript1);
+});
 
-/* Img */
 
-const copyImg = () => {
-  return src('src/img/*')
-    .pipe(dest(destDir + '/img'))
-}
+/*Styles for addons*/
+gulp.task('copyAddonsStyles', function() {
+    var aStyle1 = gulp.src(['./node_modules/@mdi/font/css/materialdesignicons.min.css'])
+        .pipe(gulp.dest('./vendors/mdi/css'));
+    var aStyle2 = gulp.src(['./node_modules/@mdi/font/fonts/*'])
+        .pipe(gulp.dest('./vendors/mdi/fonts'));
+    return merge(aStyle1, aStyle2);
+});
 
-const copyTailwindFavicons = () => {
-  return src('src/tailwind-favicons/*')
-    .pipe(dest(destDir))
-}
+/*sequence for building vendor scripts and styles*/
+gulp.task('bundleVendors', gulp.series('clean:vendors', 'buildBaseVendorStyles','buildBaseVendorScripts', 'copyAddonsStyles', 'copyAddonsScripts'));
 
-exports.default = series(
-  parallel(
-    () => concatHtml('index'),
-    () => concatHtml('tables'),
-    () => concatHtml('forms'),
-    () => concatHtml('profile'),
-    () => concatHtml('login'),
-    processJsMain,
-    processJsMainMin,
-    processJsChartSample,
-    processJsChartSampleMin,
-    copyImg,
-    copyTailwindFavicons,
-  ),
-  processCss
-)
+gulp.task('default', gulp.series('serve'));
