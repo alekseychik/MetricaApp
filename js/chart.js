a5e427587d1439448d4768affd06063001772b43
alekseychik <oleksii.belenchuk@gmail.com>
2021-11-22 00:16:17 +0200

    metrics visualization added

--- js/chart.js
+++ js/chart.js
@@ -5,10 +5,10 @@
    */
   'use strict';
   var data = {
-    labels: ["2016", "2017", "2018", "2019", "2020", "2021"],
+    labels: ["0", "30", "60", "90", "120", "150", "180"],
     datasets: [{
-      label: 'Кількість користувачів',
-      data: [10, 19, 3, 5, 2, 3],
+      label: 'Кількість днів користування',
+      data: [0, 9.0, 11.1, 12.2, 15.3, 18.4, 20.5],
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
         'rgba(54, 162, 235, 0.2)',
@@ -26,19 +26,19 @@
         'rgba(255, 159, 64, 1)'
       ],
       borderWidth: 1,
-      fill: false
+      fill: true
     }]
   };
   var options = {
     scales: {
       yAxes: [{
         ticks: {
-          beginAtZero: true
+          beginAtZero: false
         }
       }]
     },
     legend: {
-      display: false
+      display: true
     },
     elements: {
       point: {
@@ -47,6 +47,332 @@
     }
 
   };
+
+  var arpu = {
+    labels: ["Січень", "Лютий", "Березень", "Квітень", "Травень", "Червень", "Липень", "Серпень", "Вересень", "Жовтень", "Листопад"],
+    datasets: [{
+      label: 'Дохід, $',
+      data: [21.41, 20.43, 22.41, 22.84, 22.94, 23.64, 24.14, 23.70, 24.04, 24.83, 25.27],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1,
+      fill: false
+    }]
+  };
+  var arpu_options = {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: false
+        }
+      }]
+    },
+    legend: {
+      display: true
+    },
+    elements: {
+      point: {
+        radius: 0
+      }
+    }
+
+  };
+
+
+  var arrpu = {
+    labels: ["Січень", "Лютий", "Березень", "Квітень", "Травень", "Червень", "Липень", "Серпень", "Вересень", "Жовтень", "Листопад"],
+    datasets: [{
+      label: 'Дохід, $',
+      data: [27.41, 27.43, 28.41, 28.84, 28.94, 26.64, 26.14, 27.70, 28.04, 27.83, 27.27],
+      backgroundColor: [
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(255, 206, 86, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 206, 86, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(255, 206, 86, 1)'
+      ],
+      borderWidth: 1,
+      fill: false
+    }]
+  };
+  var arrpu_options = {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: false
+        }
+      }]
+    },
+    legend: {
+      display: true
+    },
+    elements: {
+      point: {
+        radius: 0
+      }
+    }
+
+  };
+
+  var ctr = {
+    labels: ["Січень", "Лютий", "Березень", "Квітень", "Травень", "Червень", "Липень", "Серпень", "Вересень", "Жовтень", "Листопад"],
+    datasets: [{
+      label: 'Дохід, $',
+      data: [27.41, 27.43, 28.41, 28.84, 28.94, 26.64, 26.14, 27.70, 28.04, 27.83, 27.27],
+      backgroundColor: [
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(153, 102, 255, 0.2)'
+      ],
+      borderColor: [
+        'rgba(54, 162, 235, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(54, 162, 235, 1)',
+      ],
+      borderWidth: 1,
+      fill: true
+    }]
+  };
+  var ctr_options = {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: false
+        }
+      }]
+    },
+    legend: {
+      display: true
+    },
+    elements: {
+      point: {
+        radius: 0
+      }
+    }
+
+  };
+
+  var dau = {
+    labels: ["Січень", "Лютий", "Березень", "Квітень", "Травень", "Червень", "Липень", "Серпень", "Вересень", "Жовтень", "Листопад"],
+    datasets: [{
+      label: 'Дохід, $',
+      data: [27.41, 27.43, 28.41, 28.84, 28.94, 26.64, 26.14, 27.70, 28.04, 27.83, 27.27],
+      backgroundColor: [
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+      ],
+      borderColor: [
+        'rgba(153, 102, 255, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(153, 102, 255, 1)'
+      ],
+      borderWidth: 1,
+      fill: true
+    }]
+  };
+  var dau_options = {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: false
+        }
+      }]
+    },
+    legend: {
+      display: true
+    },
+    elements: {
+      point: {
+        radius: 0
+      }
+    }
+
+  };
+
+  var mau = {
+    labels: ["Січень", "Лютий", "Березень", "Квітень", "Травень", "Червень", "Липень", "Серпень", "Вересень", "Жовтень", "Листопад"],
+    datasets: [{
+      label: 'Дохід, $',
+      data: [27.41, 27.43, 28.41, 28.84, 28.94, 26.64, 26.14, 27.70, 28.04, 27.83, 27.27],
+      backgroundColor: [
+        'rgba(75, 192, 192, 0.5)',
+        'rgba(75, 192, 192, 0.5)',
+        'rgba(75, 192, 192, 0.5)',
+        'rgba(75, 192, 192, 0.5)',
+        'rgba(75, 192, 192, 0.5)',
+        'rgba(75, 192, 192, 0.5)',
+        'rgba(75, 192, 192, 0.5)',
+        'rgba(75, 192, 192, 0.5)',
+        'rgba(75, 192, 192, 0.5)',
+        'rgba(75, 192, 192, 0.5)',
+        'rgba(75, 192, 192, 0.5)'
+      ],
+      borderColor: [
+        'rgba(75, 192, 192, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(75, 192, 192, 1)'
+      ],
+      borderWidth: 1,
+      fill: true
+    }]
+  };
+  var mau_options = {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: false
+        }
+      }]
+    },
+    legend: {
+      display: true
+    },
+    elements: {
+      point: {
+        radius: 0
+      }
+    }
+
+  };
+
+  var cac = {
+    labels: ["Січень", "Лютий", "Березень", "Квітень", "Травень", "Червень", "Липень", "Серпень", "Вересень", "Жовтень", "Листопад"],
+    datasets: [{
+      label: 'Вартість залучення, $',
+      data: [1.83, 2.43, 2.41, 2.04, 2.94, 1.94, 2.14, 2.70, 2.04, 2.93, 2.97],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 99, 132, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(255, 99, 132, 1)',
+        'rgba(255, 99, 132, 1)',
+        'rgba(255, 99, 132, 1)',
+        'rgba(255, 99, 132, 1)',
+        'rgba(255, 99, 132, 1)',
+        'rgba(255, 99, 132, 1)',
+        'rgba(255, 99, 132, 1)',
+        'rgba(255, 99, 132, 1)',
+        'rgba(255, 99, 132, 1)',
+        'rgba(255, 99, 132, 1)'
+      ],
+      borderWidth: 1,
+      fill: true
+    }]
+  };
+  var cac_options = {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    },
+    legend: {
+      display: true
+    },
+    elements: {
+      point: {
+        radius: 0
+      }
+    }
+
+  };
+
   var doughnutPieData = {
     datasets: [{
       data: [30, 40, 30],
@@ -83,10 +409,10 @@
     }
   };
   var areaData = {
-    labels: ["2017", "2018", "2019", "2020", "2021"],
+    labels: ["0", "30", "60", "90", "120", "150", "180"],
     datasets: [{
-      label: 'Кількість користувачів',
-      data: [12, 19, 3, 5, 2, 3],
+      label: 'Кількість днів користування',
+      data: [0, 7.8, 10.5, 12.2, 15.3, 18.4, 18.7],
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
         'rgba(54, 162, 235, 0.2)',
@@ -190,6 +516,66 @@
       type: 'bar',
       data: data,
       options: options
+    });
+  }
+
+  if ($("#arpu").length) {
+    var barChartCanvas = $("#arpu").get(0).getContext("2d");
+    // This will get the first returned node in the jQuery collection.
+    var barChart = new Chart(barChartCanvas, {
+      type: 'bar',
+      data: arpu,
+      options: arpu_options
+    });
+  }
+
+  if ($("#cac").length) {
+    var barChartCanvas = $("#cac").get(0).getContext("2d");
+    // This will get the first returned node in the jQuery collection.
+    var barChart = new Chart(barChartCanvas, {
+      type: 'line',
+      data: cac,
+      options: cac_options
+    });
+  }
+
+  if ($("#dau").length) {
+    var barChartCanvas = $("#dau").get(0).getContext("2d");
+    // This will get the first returned node in the jQuery collection.
+    var barChart = new Chart(barChartCanvas, {
+      type: 'line',
+      data: dau,
+      options: dau_options
+    });
+  }
+
+  if ($("#mau").length) {
+    var barChartCanvas = $("#mau").get(0).getContext("2d");
+    // This will get the first returned node in the jQuery collection.
+    var barChart = new Chart(barChartCanvas, {
+      type: 'line',
+      data: mau,
+      options: mau_options
+    });
+  }
+
+  if ($("#arrpu").length) {
+    var barChartCanvas = $("#arrpu").get(0).getContext("2d");
+    // This will get the first returned node in the jQuery collection.
+    var barChart = new Chart(barChartCanvas, {
+      type: 'bar',
+      data: arrpu,
+      options: arrpu_options
+    });
+  }
+
+  if ($("#ctr").length) {
+    var barChartCanvas = $("#ctr").get(0).getContext("2d");
+    // This will get the first returned node in the jQuery collection.
+    var barChart = new Chart(barChartCanvas, {
+      type: 'line',
+      data: ctr,
+      options: ctr_options
     });
   }
 
